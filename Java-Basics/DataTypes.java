581a9dc7adc1f8a54f516b07cd8a62d2dfe17d92
Abhishek Kumar <abhishek.it2028@gmail.com>
2026-08-25 20:23:15 +0530

    Add DataTypes class to demonstrate Java data types
    This file demonstrates the use of various data types in Java, including int, double, char, String, and boolean.

---
+++ Java-Basics/DataTypes.java
@@ -0,0 +1,24 @@
+public class DataTypes {
+    public static void main(String[] args) {
+
+        int age = 20;
+        int marks = 85;
+        double percentage = 99.99;
+
+        System.out.println(age);
+        System.out.println(marks);
+        System.out.println(percentage);
+
+        char grade = 'A';
+        String name = "Abhishek";
+
+        System.out.println(grade);
+        System.out.println(name);
+
+        boolean isStudent = true;
+        boolean isWorking = false;
+
+        System.out.println(isStudent);
+        System.out.println(isWorking);
+    }
+}
