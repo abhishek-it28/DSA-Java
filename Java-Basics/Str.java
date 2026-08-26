d1abefdf98d3545b6fe877b9cf8793e9e4d20fcb
Abhishek Kumar <abhishek.it2028@gmail.com>
2026-08-26 11:25:49 +0530

    Add Str.java to demonstrate string comparison

---
+++ Java-Basics/Str.java
@@ -0,0 +1,18 @@
+    class Str {
+        static void main(String[] args) {
+
+
+            String name = "Abhishek";
+            String rename = "Abhishek";
+
+            System.out.println(name.equals(rename));
+
+
+
+            String learning =  "I am learning DSA";
+
+            System.out.println(name +"\n"+ learning );
+            System.out.println(name.length());
+
+        }
+    }
